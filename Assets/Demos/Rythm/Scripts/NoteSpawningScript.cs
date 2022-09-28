9edf7715fb887f9afa60a32e3776ad2f184e8455
Cai Scheidler <scheidlercai@gmail.com>
2022-09-28 14:02:42 -0700

    Fixed Hold Note Spawning

--- Assets/Demos/Rythm/Scripts/NoteSpawningScript.cs
+++ Assets/Demos/Rythm/Scripts/NoteSpawningScript.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class NoteSpawningScript : MonoBehaviour
 {
-    public List<bool> NoteList1;
-    public List<bool> NoteList2;
-    public List<bool> NoteList3;
-    public List<bool> NoteList4;
 
-    public string EasyProgramNoteListOne; // First Number is starting position of notes, Second Number is interval of notes, Third Number is end position of notes | Write "None" If you don't want to use it
-    public string EasyProgramNoteListTwo;
-    public string EasyProgramNoteListThree;
-    public string EasyProgramNoteListFour;
+    private static int SongListLength; //Song Length in Quarters of a Second
+    public int RealSongListLength;
 
-    public int SongListLength;
+
+    //Tap Note Lists Stuff
+
+    public List<bool> TapNoteList1 = new List<bool>();
+    public List<bool> TapNoteList2 = new List<bool>();
+    public List<bool> TapNoteList3 = new List<bool>();
+    public List<bool> TapNoteList4 = new List<bool>();
+
+    public string TapEasyProgramNoteListOne; // First Number is starting position of notes, Second Number is interval of notes, Third Number is end position of notes | Write "None" If you don't want to use it
+    public string TapEasyProgramNoteListTwo;
+    public string TapEasyProgramNoteListThree;
+    public string TapEasyProgramNoteListFour;
+
+
+    //Hold Note Lists Stuff
+
+    public List<bool> HoldNoteList1 = new List<bool>();
+    public List<bool> HoldNoteList2 = new List<bool>();
+    public List<bool> HoldNoteList3 = new List<bool>();
+    public List<bool> HoldNoteList4 = new List<bool>();
+
+    public string HoldEasyProgramNoteListOne;
+    public string HoldEasyProgramNoteListTwo;
+    public string HoldEasyProgramNoteListThree;
+    public string HoldEasyProgramNoteListFour;
+
+    //Other Stuff
 
     public GameObject Note;
     public float TimeBetweenNotes;
@@ -23,59 +43,106 @@ public class NoteSpawningScript : MonoBehaviour
     private int SecondsPast = 0;
     private float period = 0;
 
+
+
+
     List<bool> ReadEasyProgramNoteList(string EasyProgramNoteList)
     {
         List<bool> NoteList = new List<bool>();
 
         if (EasyProgramNoteList != "None")
         {
-            for (int i = 0; i < SongListLength; i++)
+            for (int i = 0; i < RealSongListLength; i++)
             {
                 NoteList.Add(false);
             }
 
-            string[] NoteCode = EasyProgramNoteList.Split(" ");
 
-            for (int i = int.Parse(NoteCode[0]); i < int.Parse(NoteCode[2]); i += int.Parse(NoteCode[1]))
+
+            string[] AllNoteCode = EasyProgramNoteList.Split(",");
+            List<List<string>> AllNoteCodeList = new List<List<string>>();
+
+            foreach (string TheString in AllNoteCode)
             {
-                NoteList[i] = true;
+                string[] ThisProgrammySection = TheString.Split(" ");
+                List<string> TheListVersionOfTheProgrammySection = new List<string>(ThisProgrammySection);
+                AllNoteCodeList.Add(TheListVersionOfTheProgrammySection);
+            }
+
+
+            foreach (List<string> IntervalSet in AllNoteCodeList)
+            {
+
+                Debug.Log("From : " + IntervalSet[0] + " To : " + IntervalSet[2] + " At an interval of : " + IntervalSet[1]);
+
+
+
+                for (int i = int.Parse(IntervalSet[0]); i < int.Parse(IntervalSet[2]); i += int.Parse(IntervalSet[1]))
+                {
+                    NoteList[i] = true;
+                }
+            }
+
+        }
+        else
+        {
+            for (int i = 0; i < RealSongListLength; i++)
+            {
+                NoteList.Add(false);
             }
         }
         return NoteList;
     }
 
+
+
+
     void Start()
     {
-        NoteList1 = ReadEasyProgramNoteList(EasyProgramNoteListOne);
-        NoteList2 = ReadEasyProgramNoteList(EasyProgramNoteListTwo);
-        NoteList3 = ReadEasyProgramNoteList(EasyProgramNoteListThree);
-        NoteList4 = ReadEasyProgramNoteList(EasyProgramNoteListFour);
+
+
+        TapNoteList1 = ReadEasyProgramNoteList(TapEasyProgramNoteListOne);
+        TapNoteList2 = ReadEasyProgramNoteList(TapEasyProgramNoteListTwo);
+        TapNoteList3 = ReadEasyProgramNoteList(TapEasyProgramNoteListThree);
+        TapNoteList4 = ReadEasyProgramNoteList(TapEasyProgramNoteListFour);
+
+        HoldNoteList1 = ReadEasyProgramNoteList(HoldEasyProgramNoteListOne);
+        HoldNoteList2 = ReadEasyProgramNoteList(HoldEasyProgramNoteListTwo);
+        HoldNoteList3 = ReadEasyProgramNoteList(HoldEasyProgramNoteListThree);
+        HoldNoteList4 = ReadEasyProgramNoteList(HoldEasyProgramNoteListFour);
 
 
 
-        FullNoteList.Add(NoteList1);
-        FullNoteList.Add(NoteList2);
-        FullNoteList.Add(NoteList3);
-        FullNoteList.Add(NoteList4);
+        FullNoteList.Add(TapNoteList1);
+        FullNoteList.Add(TapNoteList2);
+        FullNoteList.Add(TapNoteList3);
+        FullNoteList.Add(TapNoteList4);
+
+        FullNoteList.Add(HoldNoteList1);
+        FullNoteList.Add(HoldNoteList2);
+        FullNoteList.Add(HoldNoteList3);
+        FullNoteList.Add(HoldNoteList4);
     }
 
     void Update()
     {
         if (period > TimeBetweenNotes)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 8; i++)
             {
                 if (FullNoteList[i][SecondsPast] == true)
                 {
 
-                    GameObject ANote = (GameObject) Instantiate(Note, new Vector3((1 * i) - 1.5f, 6, 0), Quaternion.identity);
+                     //Hold Notes Will Appear Offscreen needs fix
 
-                    if (i != 3)
+                    if (i <= 3) //TAP NOTE
                     {
+                        GameObject ANote = (GameObject)Instantiate(Note, new Vector3((1f * i) - 1.5f, 6, 0), Quaternion.identity);
                         ANote.GetComponent<MainNoteScript>().NOTE_TYPE = "TAP";
                     }
-                    else
+                    else //HOLD NOTE
                     {
+                        GameObject ANote = (GameObject)Instantiate(Note, new Vector3(((1f * i)%4) - 1.5f, 6, 0), Quaternion.identity); // ERROR: NOTES PROBABLY SPAWNING IN WRONG LANE PLS FIX (PROB ONE LANE OVER)
                         ANote.GetComponent<MainNoteScript>().NOTE_TYPE = "HOLD";
                         ANote.transform.localScale = new Vector3(1f, 3f, 0.1f);
                         var cubeRenderer = ANote.GetComponent<Renderer>();
