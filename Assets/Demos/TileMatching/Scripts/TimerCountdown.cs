b96f527b933a293db2ad02ed4cc0cc23c126e58f
Cai Scheidler <scheidlercai@gmail.com>
2022-10-03 15:31:47 -0700

    Added Timer to Tile Matching Game

---
+++ Assets/Demos/TileMatching/Scripts/TimerCountdown.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public int Seconds;
+    private int DEGREES_TO_ROTATE = 360;
+    private float DegreesRotated = 0;
+    private float DeltaTimeVsTotalTime;
+
+    void Start()
+    {
+        //Nothing lol
+    }
+
+    void Update()
+    {
+
+        //Rotate 360 degrees in "Seconds" seconds
+
+        DeltaTimeVsTotalTime = Time.deltaTime / Seconds;
+
+        DegreesRotated += DEGREES_TO_ROTATE * DeltaTimeVsTotalTime;
+
+        if (DegreesRotated <= DEGREES_TO_ROTATE)
+        {
+            this.transform.Rotate(0.0f, 0.0f, DEGREES_TO_ROTATE * DeltaTimeVsTotalTime, Space.Self);
+        }
+        
+
+    }
+}
