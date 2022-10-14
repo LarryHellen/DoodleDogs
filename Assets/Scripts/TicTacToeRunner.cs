66b1c48ebf9f2096c1e9760ec68a4cd38915e625
Cai Scheidler <scheidlercai@gmail.com>
2022-10-14 15:41:58 -0700

    Tic Tac Toe Turning Complete

--- Assets/Scripts/TicTacToeRunner.cs
+++ Assets/Scripts/TicTacToeRunner.cs
@@ -6,8 +6,26 @@ public class TicTacToeRunner : MonoBehaviour
 {
     public GameObject tile;
 
+
+    private List<List<int>> rP = new List<List<int>>()
+    {
+        new List<int>(){0,0},
+        new List<int>(){0,1},
+        new List<int>(){0,2},
+        new List<int>(){1,2},
+        new List<int>(){2,2},
+        new List<int>(){2,1},
+        new List<int>(){2,0},
+        new List<int>(){1,0},
+        new List<int>(){0,0}
+
+    };
+
+
     private List<List<GameObject>> board = new List<List<GameObject>>();
     public static int turnCounter = 0;
+
+    
 
 
     public List<List<GameObject>> Retry()
@@ -20,7 +38,7 @@ public class TicTacToeRunner : MonoBehaviour
 
             for (int j = 0; j < 3; j++)
             {
-                GameObject Tile = Instantiate(tile, new Vector3(i - 1, j, 0), Quaternion.identity);
+                GameObject Tile = Instantiate(tile, new Vector3(j, -i, 0), Quaternion.identity);
                 row.Add(Tile);
             }
 
@@ -29,6 +47,10 @@ public class TicTacToeRunner : MonoBehaviour
 
         return board;
     }
+
+    private bool hasRotated = false;
+
+    private GameObject tmp1, tmp2;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +61,101 @@ public class TicTacToeRunner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Make tiles rotate every 2 turns only once using "hasRotated" and "rotationPattern"
+        //Iterate through "rotationPattern" and set and change temp1 and temp2
+        //Hard code start change of board[1][0] -> board[0][0]
+
+        if (turnCounter % 2 == 0 && hasRotated == true)
+        {
+
+            /*
+            string aLine = "";
+
+            foreach (List<GameObject> tmp in board)
+            {
+                aLine = "";
+                foreach (GameObject tmp1 in tmp)
+                {
+                    aLine += tmp1.GetComponent<IfIveBeenClicked>().type.ToString() + " ";
+                }
+                Debug.Log(aLine);
+            }
+            */
+
+
+
+            tmp2 = board[rP[0][0]][rP[0][1]];
+
+            for (int i = 0; i < 8; i++)
+            {
+                //Debug.Log(i);
+
+                tmp1 = board[rP[i + 1][0]][rP[i + 1][1]];
+
+                board[rP[i + 1][0]][rP[i + 1][1]] = tmp2;
+
+                tmp2 = tmp1;
+            }
+
+
+
+            hasRotated = false;
+
+            /*
+            Debug.Log("-------------");
+
+            aLine = "";
+
+            foreach (List<GameObject> tmp in board)
+            {
+                aLine = "";
+                foreach (GameObject tmp1 in tmp)
+                {
+                    aLine += tmp1.GetComponent<IfIveBeenClicked>().type.ToString() + " ";
+                }
+                Debug.Log(aLine);
+            }
+            */
+
+            /*
+             * 
+             * 
+            GameObject temp = board[0][0];
+            board[0][0] = board[1][0];
+
+            GameObject temp2 = board[0][1];
+            board[0][1] = temp;
+
+
+            temp = board[0][2];
+            board[0][2] = temp2;
+
+            temp2 = board[1][2];
+            board[1][2] = temp;
+
+            temp = board[2][2];
+            board[2][2] = temp2;
+            */
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i][j].transform.position = new Vector3(j, -i, 0);
+                }
+
+            }
+
+
+        }
+
+        if (turnCounter % 2 == 1)
+        {
+            hasRotated = true;
+        }
+
         /*
 
         ####### AI CODE WILL GO HERE ######
