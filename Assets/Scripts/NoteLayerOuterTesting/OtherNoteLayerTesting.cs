09cf4372a20be4eb771d3f46ad03d825554fca7f
Slipperee-CODE <86428966+Slipperee-CODE@users.noreply.github.com>
2022-11-05 20:08:59 -0800

    Automatic Note Placement Probably Works Decently For Any Song With Some Configuration Needed

--- Assets/Scripts/NoteLayerOuterTesting/OtherNoteLayerTesting.cs
+++ Assets/Scripts/NoteLayerOuterTesting/OtherNoteLayerTesting.cs
@@ -11,6 +11,8 @@ public class OtherNoteLayerTesting : MonoBehaviour
 {
     AudioSource _audioSource;
 
+    public int sampleSize = 64;
+
     public float[] samples = new float[64];
 
     public float BPM;
@@ -28,9 +30,15 @@ public class OtherNoteLayerTesting : MonoBehaviour
 
     public int percentHigherThanCloseAvg;
 
+    public double maximumFreq;
+
+    public List<int> allDaKeys;
+
     // Start is called before the first frame update
     void Start()
     {
+        float[] samples = new float[sampleSize];
+
         interval = 60 / BPM;
 
         _audioSource = GetComponent<AudioSource>();
@@ -83,15 +91,17 @@ public class OtherNoteLayerTesting : MonoBehaviour
     }
 
 
-    void ShowAll(List<float> theList)
+    void ShowAll(List<int> theList)
     {
-        foreach (float num in theList)
+        foreach (int num in theList)
         {
             print(num);
         }
 
         print("------------------");
     }
+
+    
 
     List<int> GetSpectrumAudioSource()
     {
@@ -115,9 +125,53 @@ public class OtherNoteLayerTesting : MonoBehaviour
 
         //print(String.Join(", ", sortedDict));
 
-        List<int> allDaKeys = sortedDict.Keys.ToList();
+        allDaKeys = sortedDict.Keys.ToList();
+
+        //USE MAXIMUM FREQ AND LOOK AT 4 SECTIONS WITHIN THAT
+        //MAXIMUM FREQ SHOULD PRETTY MUCH BE LIKE SELECTION AREA
+        List<List<int>> LANES = new List<List<int>>()
+        {
+            new List<int>(),
+            new List<int>(),
+            new List<int>(),
+            new List<int>()
+        };
+
+
+
+        for (int i = 0; i < maximumFreq; i++)
+        {
+            int index = (int) Math.Floor(allDaKeys[allDaKeys.Count - i - 1] / (maximumFreq/4));
+            LANES[index].Add(allDaKeys[allDaKeys.Count - i - 1]);
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            List<float> actualFreqs = new List<float>();
+            foreach (int num in LANES[i])
+            {
+                actualFreqs.Add(freqNumAndIntensity[num]);
+            }
+
+            double avg = Queryable.Average(actualFreqs.AsQueryable());
+            float maxFreq = actualFreqs.Max(x => x);
+
+            if (maxFreq > avg * ((100 + percentHigherThanCloseAvg) / 100))
+            {
+                thisTimeAround[i] = 1;
+            }
+        }
+
+        return thisTimeAround;
+    }
+
+
+        //BELOW HAS THE RIGHT IDEA BUT DOES NOT REALLY WORK
+
+        /*
         List<int> top4Intesities = new List<int>() { allDaKeys[allDaKeys.Count - 1], allDaKeys[allDaKeys.Count - 2], allDaKeys[allDaKeys.Count - 3], allDaKeys[allDaKeys.Count - 4] }; //Keys of the Top 4 intensities
 
+        ShowAll(top4Intesities);
 
         int theIndex = 0;
 
@@ -154,7 +208,20 @@ public class OtherNoteLayerTesting : MonoBehaviour
 
             if (samples[elementPosition] > avg * ((100 + percentHigherThanCloseAvg) / 100))
             {
-                thisTimeAround[theIndex] = 1;
+
+                double someNum = elementPosition / (4/maximumFreq);
+
+                int indextoplace = (int) Math.Floor(someNum);
+                try
+                {
+                    thisTimeAround[indextoplace] = 1;
+                }
+                catch
+                {
+                    print("Too big so putting last");
+                    thisTimeAround[3] = 1;
+                }
+                
             }
 
 
@@ -164,5 +231,6 @@ public class OtherNoteLayerTesting : MonoBehaviour
         //avg intensities close to it to see if its an anomaly if so put a 1 in that lane
 
         return thisTimeAround;
-    }
+        */
 }
+
