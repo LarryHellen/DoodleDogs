ba7e000838fe67ff4ba570cd1206e68aaea72717
Cai Scheidler <scheidlercai@gmail.com>
2022-12-06 15:29:34 -0800

    Pacman - Get Component can't find a script

---
+++ Assets/Scripts/Pacman/TileScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TileScript : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;
+
+    public int xCoord;
+    public int yCoord;
+
+    public GameObject mapDesigner;
+
+    void PlaceTile()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (this.GetComponent<Collider2D>().OverlapPoint(mousePosition))
+            {
+                
+                if (mapDesigner.GetComponent<MapDesigner>().mapList[yCoord][xCoord] = 1) //tmp1.GetComponent<IfIveBeenClicked>().type.
+                {
+                    mapDesigner.GetComponent<MapDesigner>().mapList[yCoord][xCoord] = 0;
+                    spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                }
+                else
+                {
+                    mapDesigner.GetComponent<MapDesigner>().mapList[yCoord][xCoord] = 1;
+                    spriteRenderer.color = new Color(0f, 0f, 0f, 1f);
+                }
+                
+            }
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlaceTile();
+    }
+}
