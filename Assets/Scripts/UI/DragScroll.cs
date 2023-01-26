2835b8241d060898d863b14e562ae23c62220f8a
Benjamin Anderson <benjaminbairdanderson@gmail.com>
2023-01-26 14:41:06 -0800

    Level Select in

---
+++ Assets/Scripts/UI/DragScroll.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScroll : MonoBehaviour
+{
+    public GameObject background;
+    public Vector3 startPoint = new Vector3(0, 0, 0);
+    public int b = 0;
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        startPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        Debug.Log("e");
+    }
+    private void OnMouseUp()
+    {
+        startPoint = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startPoint != new Vector3(0, 0, 0))
+        {
+            background.transform.position -= new Vector3(0, (Input.mousePosition.y - startPoint.y)/2, 0);
+            startPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        }
+        if (background.transform.position.y < b)
+        {
+            background.transform.position = new Vector3(background.transform.position.x, b, background.transform.position.z);
+        }
+    }
+}
