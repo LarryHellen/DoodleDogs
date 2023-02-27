917d00ac3ac92e78051654e8633631471bc094e2
Cai Scheidler <scheidlercai@gmail.com>
2023-02-27 15:32:22 -0800

    Rhythm Game - Refactor is underway

---
+++ Assets/Scripts/Testing/RhythmTesting/Properly Usable Scripts/BasicNoteObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicNoteObject : MonoBehaviour
+{
+    //Get ScreenWidth ("Screen.width")
+    //Get ScreenHeight
+    //Init Columns (Get from main spanwer script)
+    //Init NotePercentLengthOfScreen (Get from main spanwer script)
+    //Init NoteWidth
+    //Init NoteHeight
+    //Init IntervalLength (Get from main spanwer script)
+    //Init SpaceBetweenNotes (Get from main spanwer script)
+    //Init PublicDeltaTime (Get from main spanwer script)
+    //Init TimeElapsedSinceLastInterval
+    //Init ScreenHeightPercentForNoteHide (Get from main spanwer script)
+    //Init ScreenHeightPercentForNoteToLandOnBeat (Get from main spanwer script)
+
+
+    // OPACITY //
+    //Init ScreenHeightPercentForNoteShow (Get from main spanwer script)
+    //Init InitialOpacity
+    //Init EndOpacity
+    //Init SpriteRenderer
+
+
+
+
+
+    void Start()
+    {
+        //Set all variables for proper positioning and size
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
+
+
+//THINGS I NEED TO FIGURE OUT
+// - HOLD NOTE SPAWNING SYSTEM (OLD METHOD? FIND A NEW METHOD?)
+// - BASIC NOTE AND HOLD NOTE TAPPING SYSTEMS
+// - NOTE MOVEMENT SO THAT NOTES LAND ON A CERTAIN SCREEN HEIGHT PERCENTAGE ON THE BEAT
+// - NOTE PATTERN CREATOR + DISPLAYER
+// - NEW HOLD AND BASIC NOTE PREFABS
---
+++ Assets/Scripts/Testing/RhythmTesting/Properly Usable Scripts/HoldNoteObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldNoteObject : MonoBehaviour
+{
+
+    //Init HoldNoteLength
+    //Init HoldNoteLengthConstant (Should be a percent of screen height)
+    //Init HoldNoteBaseTime (1 Interval, Get from main spanwer script)
+    //Init HoldNoteBaseTimeConstant
+
+
+
+    void Start()
+    {
+        //When note spawn, scale to the correct size
+    }
+
+
+    void Update()
+    {
+
+    }
+}
