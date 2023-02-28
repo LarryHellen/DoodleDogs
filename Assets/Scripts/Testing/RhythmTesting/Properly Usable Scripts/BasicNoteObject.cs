5b6b49b74e3355c2885a3c38c9c7450c06234cdf
Cai Scheidler <scheidlercai@gmail.com>
2023-02-28 15:32:25 -0800

    More Rhythm Pseudo Code

--- Assets/Scripts/Testing/RhythmTesting/Properly Usable Scripts/BasicNoteObject.cs
+++ Assets/Scripts/Testing/RhythmTesting/Properly Usable Scripts/BasicNoteObject.cs
@@ -15,6 +15,9 @@ public class BasicNoteObject : MonoBehaviour
     //Init TimeElapsedSinceLastInterval
     //Init ScreenHeightPercentForNoteHide (Get from main spanwer script)
     //Init ScreenHeightPercentForNoteToLandOnBeat (Get from main spanwer script)
+    //Init NoteSpeedInIntervals (Get from main spanwer script)
+
+    //Init TimeBeenTapped
 
 
     // OPACITY //
@@ -29,13 +32,39 @@ public class BasicNoteObject : MonoBehaviour
 
     void Start()
     {
-        //Set all variables for proper positioning and size
+        //Set all variables for proper positioning and size (change sizze only if HoldNoteObject script not attached)
     }
 
 
     void Update()
     {
-        
+        //NOTE - MOVEMENT
+
+
+        //USE PHONE PHOTO TO MAKE THIS SYSTEM
+
+
+        //END OF "NOTE - MOVEMENT"
+
+
+
+        //NOTE - WINNING AND LOSING
+
+
+        //if HoldNoteObject script is attached and object being clicked, increase a TimeBeenTapped variable by deltaTime
+        //if TimeBeenTapped is greater than this objects HoldTime, [Destroy this object and give the player score] <- Seperate function?
+
+        //if HoldNoteObject script is attached and object not being clicked
+        //Set TimeBeenTapped to 0
+
+
+        //else if (HoldNoteObject script not attached and object being clicked), [Destroy this object and give the player score] <- Seperate function?
+
+
+        //if object has passed ScreenHeightForNoteHide, [Destroy this object and set the player's score to 0]
+
+
+        //END OF "NOTE - WINNING AND LOSING"
     }
 }
 
@@ -46,3 +75,4 @@ public class BasicNoteObject : MonoBehaviour
 // - NOTE MOVEMENT SO THAT NOTES LAND ON A CERTAIN SCREEN HEIGHT PERCENTAGE ON THE BEAT
 // - NOTE PATTERN CREATOR + DISPLAYER
 // - NEW HOLD AND BASIC NOTE PREFABS
+// - DELAY THE START OF AUDIO BY (MaxHoldNoteLength + TimeToReachOnBeatPosition) number of Intervals
