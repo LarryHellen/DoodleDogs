d0b2762a1b40d4e65b0a69cebafd5d6c80ed7d25
LarryHellen <lauri.hellen@gmail.com>
2023-03-08 11:33:13 -0800

    Fixed bug with tile matching on builds

--- Assets/Scripts/TileMatching/CounterHolder.cs
+++ Assets/Scripts/TileMatching/CounterHolder.cs
@@ -8,7 +8,7 @@ public class CounterHolder : MonoBehaviour
     public GameObject[] counterArray;
     public float yDistance;
     public float xDistance;
-    private Vector3 tempPos;
+    //private Vector3 tempPos;
     private Board board;
     public int goals;
 
@@ -21,17 +21,17 @@ public class CounterHolder : MonoBehaviour
 
     public void Reset(){
         foreach(Transform child in transform){
-            Destroy(child.gameObject);
+            child.GetComponent<MatchCounter>().Reset();
         }
         SetUp();
     }
 
     public void SetUp(){
-        tempPos = gameObject.transform.position;
+        /*tempPos = gameObject.transform.position;
         counterArray[0] = addCounter(tempPos,goals,"Bread Dot",false);
         counterArray[1] = addCounter(tempPos,goals,"Cheese Dot",true);
         counterArray[2] = addCounter(tempPos,goals,"Butter Dot",false);
-        counterArray[3] = addCounter(tempPos,goals,"Ham Dot",false);
+        counterArray[3] = addCounter(tempPos,goals,"Ham Dot",false);*/
     }
 
     public void Update()
@@ -42,7 +42,7 @@ public class CounterHolder : MonoBehaviour
         }
     }
 
-    GameObject addCounter(Vector3 pos, int num, string type,bool newRow){
+    /*GameObject addCounter(Vector3 pos, int num, string type,bool newRow){
         GameObject tempCounter = Instantiate(counterPrefab,pos,Quaternion.identity,gameObject.transform);
         tempCounter.GetComponent<MatchCounter>().Create(num,type);
         if (newRow)
@@ -55,7 +55,7 @@ public class CounterHolder : MonoBehaviour
             tempPos.x += xDistance;
         }
         return tempCounter;
-    }
+    }*/
 
     public void addMatch(string type, int numberToAdd){
         foreach(GameObject counter in counterArray){
