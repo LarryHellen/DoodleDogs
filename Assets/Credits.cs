5a1ea9097b7368f8f32874ee4545c27d4130be07
Benjamin Anderson <benjaminbairdanderson@gmail.com>
2023-04-14 15:25:03 -0700

    Credits menu done

--- Assets/Credits.cs
+++ Assets/Credits.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    public GameObject creditsText;
+    public Slider slideBar;
     // Start is called before the first frame update
     public void valueChanged() {
-
+        creditsText.transform.position = new Vector3(creditsText.transform.position.x, 1344 - 900 + slideBar.value * 1400, creditsText.transform.position.z);
     }
 
 }
