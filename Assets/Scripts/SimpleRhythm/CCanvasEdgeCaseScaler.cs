07b1c56e3a2f13dda845d3aefa485baa25bc78dd
Cai Scheidler <scheidlercai@gmail.com>
2023-05-08 15:31:55 -0700

    R - Scaling works for every phone except 1920x1080

---
+++ Assets/Scripts/SimpleRhythm/CCanvasEdgeCaseScaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CCanvasEdgeCaseScaler : MonoBehaviour
+{
+    private Canvas canvas;
+    private RectTransform canvasRt;
+
+
+    void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        canvasRt = GetComponent<RectTransform>();
+
+        float canvasWidth = canvasRt.sizeDelta.x;
+        float canvasHeight = canvasRt.sizeDelta.y;
+
+        if (canvasWidth*2 > canvasHeight)
+        {
+            CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+
+            //canvasScaler.referenceResolution = new Vector2(1080, 1920);
+        }
+    }
+}
